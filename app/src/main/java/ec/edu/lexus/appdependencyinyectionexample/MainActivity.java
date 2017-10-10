4b5562861df500cd1eabe1f90d0ec75cda0b0b30
Alexis Bautista <rhack_100@outlook.com>
2017-10-10 17:02:03 -0500

    :Exercise

--- app/src/main/java/ec/edu/lexus/appdependencyinyectionexample/MainActivity.java
+++ app/src/main/java/ec/edu/lexus/appdependencyinyectionexample/MainActivity.java
@@ -3,11 +3,34 @@ package ec.edu.lexus.appdependencyinyectionexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MessageService messageService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        messageService = new MessageService(this, "Hello!", "Good bye!!", "What's your name???");
+    }
+
+    @OnClick(R.id.hi_button)
+    public void hi() {
+        messageService.sayHi();
+    }
+
+    @OnClick(R.id.bye_button)
+    public void bye() {
+        messageService.sayBye();
+    }
+
+    @OnClick(R.id.ask_button)
+    public void ask() {
+        messageService.askSomething();
     }
 }
---
+++ app/src/main/java/ec/edu/lexus/appdependencyinyectionexample/MessageService.java
@@ -0,0 +1,36 @@
+package ec.edu.lexus.appdependencyinyectionexample;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Alexis on 10/10/2017.
+ */
+
+public class MessageService {
+
+    private Context context;
+    private String hi;
+    private String bye;
+    private String question;
+
+    public MessageService(Context context, String hi, String bye, String question) {
+        this.context = context;
+        this.hi = hi;
+        this.bye = bye;
+        this.question = question;
+    }
+
+    public void sayHi() {
+        Toast.makeText(context, hi, Toast.LENGTH_SHORT).show();
+    }
+
+    public void sayBye() {
+        Toast.makeText(context, bye, Toast.LENGTH_SHORT).show();
+    }
+
+    public void askSomething() {
+        Toast.makeText(context, question, Toast.LENGTH_SHORT).show();
+    }
+}
+
