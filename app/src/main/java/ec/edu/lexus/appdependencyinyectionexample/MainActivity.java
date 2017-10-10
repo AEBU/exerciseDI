8ccfb8f1f360a9d938e194ffe4c0d104a58e4d10
Alexis Bautista <rhack_100@outlook.com>
2017-10-10 16:26:06 -0500

    :init

---
+++ app/src/main/java/ec/edu/lexus/appdependencyinyectionexample/MainActivity.java
@@ -0,0 +1,13 @@
+package ec.edu.lexus.appdependencyinyectionexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
